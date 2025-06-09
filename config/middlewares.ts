de47641dd1e1527e7ae19e17dded0c64055930aa
Mark Marcel <freelancemarcel18@gmail.com>
2025-06-09 13:17:26 +0000

    schema & media - added config to upload files to google cloud storage

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,7 +1,19 @@
 export default [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'default-src': ["'self'"],
+          'img-src': ["'self'", 'data:', 'blob:', 'https://storage.googleapis.com'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
---
+++ src/api/website-image/services/website-image.ts
@@ -0,0 +1,7 @@
+/**
+ * website-image service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::website-image.website-image');
